d7f1614d26213f243b8f0452fb071f25ef2e4494
Favour Ifurukpe <tinafavour2018@gmail.com>
2024-06-17 22:57:30 +0100

    change

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
+const hov = document.getElementById("hov")
 const text = document.getElementById("text")
+const hov2 = document.getElementById("hov2")
 const text2 = document.getElementById("text2")
+const hov3 = document.getElementById("hov3")
+const text3 = document.getElementById("text3")
 
-text.addEventListener("mouseout", function(){
-    text.textContent = "Login Page";
-    text.classList.add("first")
+
+
+hov.addEventListener("mouseout", function(){
+    text.innerHTML = `<p>Login Page</p>`;
     text.classList.remove("change")
+    text.classList.add("first")
+    
 })
-text.addEventListener("mouseover", function(){
-    text.textContent = "Login page that verifies if what you signed in with is the same with what you logged in with";
+hov.addEventListener("mouseover", function(){
+    text.innerHTML=`
+    <p>Login page that verifies if what you signed in with is the same with what you logged in with</p> 
+    <button  class=" text-white text-[15px] border-4 border-[#6E07F3] px-[2rem] py-2 hover:bg-[#6E07F3] rounded-full" >Visit Website</button>`
     text.classList.add("change")
+    
+    
+
 })
 // second grid 
 text2.addEventListener("mouseout", function(){
